a7bae1339307d50c0bfd8823b2b3889b6a142572
Jheivy Stiven Moreno Silva <jheivymoreno@gmail.com>
2025-08-10 17:29:22 -0500

    Correcion de dependencias para electron

--- src/multiplatform/native/electron/store.ts
+++ src/multiplatform/native/electron/store.ts
@@ -1,27 +1,3 @@
-import type { Store as StoreInterface } from '@data/storage';
-import { default as ElectronStore } from 'electron-store';
+import { storage } from '@data/storage';
 
-function createElectronStore(): StoreInterface {
-	const store = new ElectronStore();
-
-	return {
-		set<T>(key: string, value: T): void {
-			store.set(key, value);
-		},
-		get<T>(key: string): T | null {
-			const value = store.get(key);
-			return value === undefined ? null : (value as T);
-		},
-		delete(key: string): void {
-			store.delete(key);
-		},
-		has(key: string): boolean {
-			return store.has(key);
-		},
-		clear(): void {
-			store.clear();
-		},
-	};
-}
-
-export const store = createElectronStore();
+export const store = storage;
