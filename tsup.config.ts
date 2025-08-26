4e08d906169ca07e7a65413ad4ad85cf3edcb56d
Jheivy Stiven Moreno Silva <jheivymoreno@gmail.com>
2025-08-25 23:52:58 -0500

    Correción de exportación de plugins

--- tsup.config.ts
+++ tsup.config.ts
@@ -1,19 +1,34 @@
 import { defineConfig } from 'tsup';
 
-export default defineConfig({
-	entry: ['src/**/*.ts'],
-	format: ['esm'],
-	splitting: true,
-	minify: true,
-	dts: true,
-	clean: true,
-	target: 'esnext',
-	outDir: 'dist/src',
-	platform: 'browser',
-	shims: true,
-	bundle: true,
-	treeshake: true,
-	external: ['fs', 'os', 'path', 'child_process', 'crypto', 'tty'],
-	tsconfig: './tsconfig.json',
-	onSuccess: 'node scripts/copy-types.mjs',
-});
+export default defineConfig([
+	// Bundle para la app principal (todo menos plugins)
+	{
+		entry: ['src/**/*.ts', '!src/plugins/**'],
+		format: ['esm'],
+		splitting: true,
+		minify: true,
+		dts: true,
+		clean: true,
+		target: 'esnext',
+		outDir: 'dist/src',
+		platform: 'browser',
+		shims: true,
+		bundle: true,
+		treeshake: true,
+		external: ['fs', 'os', 'path', 'child_process', 'crypto', 'tty', 'esbuild'],
+		tsconfig: './tsconfig.json',
+		onSuccess: 'node scripts/copy-types.mjs',
+	},
+
+	// Compilación para plugins (sin bundle, tal cual TS → JS)
+	{
+		entry: ['src/plugins/**/*.ts'],
+		format: ['cjs', 'esm'],
+		bundle: false,
+		dts: true,
+		external: ['esbuild'],
+		outDir: 'dist/plugins',
+		platform: 'node',
+		tsconfig: './tsconfig.json',
+	},
+]);
